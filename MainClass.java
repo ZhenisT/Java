29655fc37c0f2d042dffcb4f657ee422ed95c0f8
ZhenisT <corpsolut@mail.ru>
2019-04-29 01:56:16 +0600

    Написал метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false

--- MainClass.java
+++ MainClass.java
@@ -13,6 +13,17 @@ public class MainClass {
         boolean bl = false;
         System.out.println("short " + s + " int " + i + " long " + l + " float " + f + " double " + d + " char " + c);
         System.out.println(rez(10, 2, 10, 2));
+        int x = 15, y = 10, sum;
+        sum = x + y;
+        if ((sum >= 10) && (sum <= 20))
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+
     }
 
     public static float rez(int a, int b, int c, int d){
