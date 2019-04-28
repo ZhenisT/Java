af7f0e1ef4ca25a0e2ab7f5e351b0385654fde7e
ZhenisT <corpsolut@mail.ru>
2019-04-29 01:20:57 +0600

    Добавил и инициализировал переменные

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,16 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("Переменные:");
+        byte b = 125;
+        System.out.println("b = " + b);
+        short s = -32768;
+        int i = 50000;
+        long l = 123456789L;
+        float f = 15.75f;
+        double d = -157.3456;
+        char c = 'c';
+        boolean bl = false;
+        System.out.println("short " + s + " int " + i + " long " + l + " float " + f + " double " + d + " char " + c);
     }
 }
