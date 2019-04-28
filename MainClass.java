c6dec9f59f361668dee6026bf885b1329266fd19
ZhenisT <corpsolut@mail.ru>
2019-04-29 02:05:34 +0600

    Написал метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль положительное ли число передали, или отрицательное

--- MainClass.java
+++ MainClass.java
@@ -23,6 +23,13 @@ public class MainClass {
         {
             System.out.println("false");
         }
+	int pol = -10;
+        if (pol < 0) {
+            System.out.println("Число отрицательное");
+        } else
+        {
+            System.out.println("Число положительное");
+        }
 
     }
 
